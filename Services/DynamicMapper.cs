7976b0a4161278cd3ba72d09e15f6272ad076aa1
wendolyn <wendolynpedraza@gmail.com>
2024-12-19 11:03:06 -0600

    Logica de camiones: + Interfaz ICamiones + Camiones service +DynamicMapper +DTO

---
+++ Services/DynamicMapper.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+
+namespace TransportesAPI.Services
+{
+    public class DynamicMapper
+    {
+        //metodo que mapea de forma dinamica diferentes tipos de obetos (modelos originale sa DTO y viceversa
+        public static TDestination Map<Tsource, TDestination>(Tsource source) 
+            where Tsource : class //se declara una clase abstracta como tipo de dato de entrada
+            where TDestination : class, new()//se declara una clase abstracta como tipo de dato de salida
+
+            
+        {
+            //valida si existe y contiene informacion la cla sdel origel
+            if (source == null) throw new ArgumentNullException("source");
+            var destination = new TDestination(); //creo una instacia del objeto de salida 
+
+            //recuperar las propiedades (los atributos de mis elementos) usando la biblioteca system.reflexion
+            //Mediante reflexión, puedes acceder a las propiedades de un tipo (clase, estructura, etc.) en tiempo de ejecución, incluso si no conoces el tipo exacto en tiempo de compilación.
+            //GetProperties: Devuelve un array con todas las propiedades públicas del tipo especificado.
+            //BindingFlags: Opciones que especifican qué miembros buscar(públicos, privados, estáticos, etc.).
+            //using System.Reflection;
+
+            var sourcePropertis = typeof(Tsource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            var destinationProperties= typeof(TDestination).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            //rrecorro todos los atributos de origen para equipararlos con el objetp de salida 
+            foreach(var sourceProperty in destinationProperties)
+            {
+                var destinationProperty = destinationProperties.FirstOrDefault(dp => dp.Name.ToLower() == sourceProperty.Name.ToLower()
+                &&
+                dp.PropertyType==sourceProperty.PropertyType);
+                if(destinationProperty!=null && destinationProperty.CanWrite)
+                {
+                    //GETValue: lee el valor actual de la propiedad del origen al destino
+                    //SetValue: Establece un nuevo vslor a la propiedad de un objeto
+                    var value = sourceProperty.GetValue(source);
+                    destinationProperty.SetValue(destination, value);
+                }
+            }
+            //retorno el nuevo tipo de objeto ya mapeado
+            return destination;
+        }
+
+
+    }
+}
---
+++ Services/ICamiones.cs
@@ -0,0 +1,139 @@
+﻿using DTO;
+using TransportesAPI.Models;
+
+namespace TransportesAPI.Services
+{
+    public interface ICamiones
+    {
+        //es una estructura que define un contrato o conjunto de métodos y
+        //propiedades que una clase debe implementar.
+        //Una interfaz establece un conjunto de requisitos que cualquier clase
+        //que la implemente debe seguir. Estos requisitos son declarados en la
+        //interfaz en forma de firmas de métodos y propiedades,
+        //pero la interfaz en sí misma no proporciona ninguna implementación
+        //de estos métodos o propiedades.Es responsabilidad de las clases que
+        //implementan la interfaz proporcionar las implementaciones concretas de
+        //estos miembros.
+
+        //Las interfaces son útiles para lograr la abstracción y la reutilización
+        //de código en C#.
+
+        //GET
+        List<Camiones_DTO> GetCamiones();
+        //GETbyId
+        Camiones_DTO GetCamiones(int id);
+        //INSERT (POST)
+        string InsertCamion(Camiones_DTO camion);
+        //UPDATE (PUT)
+        string UpdateCamion(Camiones_DTO camion);
+        //DELETE (DELETE)
+        string DeleteCamion(int id);
+    }
+
+    //la clase wue implementa la interfaz y declara la imprlementacion d ela ligica d elos etodos existentes 
+    public class CamionesService : ICamiones
+    {
+        //variable para crear el contexto (Inyeccion de dependencias )
+        private readonly TransportesContext _context;
+        //constructor para inicializar el contexto
+        public CamionesService(TransportesContext context)
+        {
+            _context = context;
+        }
+        //Implementacion de metodos
+        public string DeleteCamion(int id)
+        {
+            try
+            {
+                //obtengo el camion de base de datos
+                Camiones _camion = _context.Camiones.Find(id);
+                if(_camion == null)
+                {
+                    return $"No se encontro algun objeto con identificaador {id}";
+                }
+                //remuevo el objeto del contexto
+                _context.Camiones.Remove(_camion);
+                //inpacto la BD
+                _context.SaveChanges();
+                //respondo
+                return $"Camion {id} eliminado con exito";
+            }
+            catch(Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+
+        public List<Camiones_DTO> GetCamiones()
+        {
+            try
+            {
+                //lista de camiones del original
+                List<Camiones> lista_original = _context.Camiones.ToList();
+                //lista de DTOs
+                List<Camiones_DTO> lista_salida = new List<Camiones_DTO>();
+                //recorro cada camion y genero un nuvo DTO con DinamycMapper
+                foreach (var cam in lista_original) {
+                    //usamos el dinamycmapper para convertitr
+                    Camiones_DTO  DTO= DynamicMapper.Map<Camiones, Camiones_DTO>(cam);
+                    lista_salida.Add(DTO);
+                }
+                //retorno la lista con los objetos ya mapeados
+                return lista_salida;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception();
+            }
+        }
+
+        public Camiones_DTO GetCamiones(int id)
+        {
+            Camiones origen = _context.Camiones.Find(id);
+            //DynamicMapper
+            Camiones_DTO resultado = DynamicMapper.Map<Camiones, Camiones_DTO>(origen);
+            return resultado;
+        }
+
+        public string InsertCamion(Camiones_DTO camion)
+        {
+            try
+            {
+                //creo un camion del modelo original
+                Camiones _camion = new Camiones();
+                //asigno los valores del objrto DTO del parametro al objeto del modelo original
+                _camion = DynamicMapper.Map<Camiones_DTO, Camiones>(camion);
+                //añadimo el objeto al contexto
+                _context.Camiones.Add(_camion);
+                //impactamos la BD
+                _context.SaveChanges();
+                //respondo 
+                return "Camion insertado con exito";
+            }catch(Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+
+        public string UpdateCamion(Camiones_DTO camion)
+        {
+            try
+            {
+                //creo un camion del modelo original
+                Camiones _camion = new Camiones();
+                //asigno los valores del objrto DTO del parametro al objeto del modelo original
+                _camion = DynamicMapper.Map<Camiones_DTO, Camiones>(camion);
+                //modifico el estado del modelo en el contexto 
+                _context.Entry(_camion).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                //impactamos la BD
+                _context.SaveChanges();
+                //respondo 
+                return $"Camion {_camion.ID_Camion} Actualizado con exito";
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+    }
+}
